0d82b1e94c3f2a8a936c56a817c1db520da2d92b
Muhammad Razif Rizqullah <razifrizqullah@gmail.com>
2021-08-10 08:57:20 +0700

    adding object and manager

---
+++ CSVManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace GarbageCollection {
+  class CSVManager {
+    private ArrayList _tweets;
+    private StreamReader _csvStreamer;
+    internal CSVManager() {
+      _tweets = new ArrayList();
+    }
+
+    internal ArrayList ReadUsingFileReadLines(string filePath) {
+      IEnumerable<string> rows = File.ReadLines(filePath);
+      foreach (string row in rows.Skip(1)) {
+        string[] queryArray = ParseStringToArray(row);
+        try {
+          Tweet tweet = new Tweet(queryArray);
+          _tweets.Add(tweet);
+        }
+        catch (Exception createTweetObjectFailed) {
+          Console.WriteLine($"{queryArray[0]} Message: { createTweetObjectFailed.Message } ");
+        }
+
+      }
+      return _tweets;
+    }
+
+    // outputnya List of string System.IO, File.ReadAllText
+
+    private string[] ParseStringToArray(string text) {
+      text.Replace(",,", ", ,");
+      string[] queryArray = text.Split(',');
+      return queryArray;
+    }
+
+    private string[] DynamicObjectToArray(dynamic obj) {
+      string[] queryArray = new string[] {
+        obj.ID, obj.lang, obj.Date, obj.Source, obj.len, obj.Tweet, obj.Likes, 
+        obj.RTs, obj.Hashtags, obj.UserMentionNames, obj.UserMentionID, 
+        obj.Name, obj.Place, obj.Followers, obj.Friends
+      };
+      return queryArray;
+    }
+  }
+}
---
+++ tweet.cs
@@ -0,0 +1,53 @@
+﻿namespace GarbageCollection {
+  class Tweet {
+    private string _id;
+    private string _lang;
+    private string _date;
+    private string _source;
+    private string _len;
+    private string _fulltext;
+    private string _likes;
+    private string _rts;
+    private string _hashtags;
+    private string _usermentionnames;
+    private string _usermentionid;
+    private string _name;
+    private string _place;
+    private string _followers;
+    private string _friends;
+
+    public string ID { get { return _id; } }
+    public string Lang { get { return _lang; } }
+    public string Date { get { return _date; } }
+    public string Source { get { return _source; } }
+    public string Len { get { return _len; } }
+    public string FullText { get { return _fulltext; } }
+    public string Likes { get { return _likes; } }
+    public string RTs { get { return _rts; } }
+    public string Hashtags { get { return _hashtags; } }
+    public string UserMentionNames { get { return _usermentionnames; } }
+    public string UserMentionID { get { return _usermentionid; } }
+    public string Name { get { return _name; } }
+    public string Place { get { return _place; } }
+    public string Followers { get { return _followers; } }
+    public string Friends { get { return _friends; } }
+
+    public Tweet(string[] inputArray) {
+      _id = inputArray[0];
+      _lang = inputArray[1];
+      _date = inputArray[2];
+      _source = inputArray[3];
+      _len = inputArray[4];
+      _fulltext = inputArray[5];
+      _likes = inputArray[6];
+      _rts = inputArray[7];
+      _hashtags = inputArray[8];
+      _usermentionnames = inputArray[9];
+      _usermentionid = inputArray[10];
+      _name = inputArray[11];
+      _place = inputArray[12];
+      _followers = inputArray[13];
+      _friends = inputArray[14];
+    }
+  }
+}
